bd6ed14bc9dae4106b459a3c2871fb8e09b01b7c
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2023-03-19 19:26:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const {MongoClient} = require('mongodb');
 const PORT = process.env.PORT || 3000
 const uri = process.env.dbURL;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 
 
 // Fetch All Employee
@@ -16,41 +16,6 @@ app.get('/getEmp', async (req, res) => {
 	let cursor = await client.db("ESD").collection("Employee").find().toArray()
 	return res.send(cursor)
 });
-
-// Add Employee
-app.post('/addEmp', async (req, res) => {
-	client.db("ESD").collection("Employee").insertOne(req.body)
-	.then(res.status(200).send("User Addded "))
-	.catch((e)=>res.send(e))
-})
-
-// Update/Insert Employee
-app.put('/updEmp', async (req, res) => {
-	client.db("ESD").collection("Employee").updateOne({"PHONE_NUMBER": req.body.PHONE_NUMBER}, 
-		{"$set": req.body},{upsert:true}).then(data => {
-			if (data.modifiedCount == 1){
-				res.status(200).send("User Updated!")
-			}
-			else{
-				res.status(200).send("User Created")
-			}
-		})
-	})
-
-// Delete Employee
-app.delete('/delEmp',async (req,res)=>{
-	client.db("ESD").collection("Employee").deleteOne({"PHONE_NUMBER": req.body.PHONE_NUMBER})
-	.then(data=> {
-		if (data.deletedCount==1){
-
-			res.status(200).send("Employee Record Deleted")
-		}
-		else{
-			res.status(404)
-			.send("Employee Not Found!")
-		}
-	})
-})
 
 
 client.connect(err => {
